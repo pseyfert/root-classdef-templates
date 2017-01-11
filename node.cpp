95c8afd4c792077fd685b5300900490f52d63aff
Paul Seyfert <paul.seyfert@mib.infn.it>
2017-01-11 14:37:54 +0100

    added function friend

--- node.cpp
+++ node.cpp
@@ -4,6 +4,13 @@ template <typename NT>
 node<NT>::~node() {
 }
 
+template <typename NT>
+std::ostream& operator<<( std::ostream& os, const node<NT>& thenode ) {
+  os << thenode.f;
+  return os;
+}
+
 class dnode;
 template class node<dnode>;
+template std::ostream& operator<<( std::ostream& os, const node<dnode>& thenode ) ;
 templateClassImp(node);
--- node.h
+++ node.h
@@ -1,9 +1,19 @@
 #pragma once
 #include "Rtypes.h"
+#include <iostream>
+
+template <typename NT>
+class node;
+
+template <typename NT>
+std::ostream& operator<<( std::ostream& os, const node<NT>& thenode );
 
 template <typename NT>
 class node {
+  friend std::ostream& operator<< <>( std::ostream& os, const node<NT>& thenode );
   public:
     virtual ~node();
     ClassDef(node,0);
+  private:
+    float f;
 };
